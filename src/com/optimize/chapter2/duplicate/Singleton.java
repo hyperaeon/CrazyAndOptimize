cdb947259c47ed49213468c75ea6721309d91ec6
Oliver <flyloeswing@163.com>
2015-05-10 15:27:39 +0800

    Duplicate of chapter2

---
+++ src/com/optimize/chapter2/duplicate/Singleton.java
@@ -0,0 +1,18 @@
+package com.optimize.chapter2.duplicate;
+
+public class Singleton {
+
+	private static Singleton instance = new Singleton();
+	
+	private Singleton() {
+		System.out.println("Singleton is created");
+	}
+	
+	public static Singleton getInstance() {
+		return instance;
+	}
+	
+	public static void createString() {
+		System.out.println("create String in Singletong");
+	}
+}
