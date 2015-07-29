cc95054db63c6a81c331b678f52cb6bc04b5f391
a588727 <Li, Yong@StateStreet.com>
2015-07-29 16:22:13 +0800

    Add builder duplicate.

---
+++ src/com/designpattern/builder/duplicate/Builder.java
@@ -0,0 +1,25 @@
+package com.designpattern.builder.duplicate;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.designpattern.abstractfactory.duplicate.EmailSender;
+import com.designpattern.abstractfactory.duplicate.Sender;
+import com.designpattern.abstractfactory.duplicate.SmsSender;
+
+public class Builder {
+
+	private List<Sender> senderList = new ArrayList<Sender>();
+	
+	public void produceSmsSender(int count) {
+		for (int i = 0; i < count; i++) {
+			senderList.add(new SmsSender());
+		}
+	}
+	
+	public void produceEmailSender(int count) {
+		for (int i = 0; i < count; i++) {
+			senderList.add(new EmailSender());
+		}
+	}
+}
