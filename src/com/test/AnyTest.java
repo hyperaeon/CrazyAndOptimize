760aa890ec9fea9119301fa824de4b95560a6889
a588727 <Li, Yong@StateStreet.com>
2015-07-30 14:11:23 +0800

    Add interpreter duplicate and AnyTest.

---
+++ src/com/test/AnyTest.java
@@ -0,0 +1,18 @@
+package com.test;
+
+import java.util.regex.Pattern;
+
+public class AnyTest {
+
+	public static void main(String[] args) {
+	}
+	
+	
+	private static void stringTest() {
+		
+	}
+	
+	private static void dateTest() {
+		
+	}
+}
