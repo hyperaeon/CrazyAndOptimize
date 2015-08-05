9afa0b5b6ef41659f690c939f2ffb53775529872
a588727 <Li, Yong@StateStreet.com>
2015-08-05 13:07:46 +0800

    Add designpattern->state->duplicate.

---
+++ src/com/designpattern/state/duplicate/State.java
@@ -0,0 +1,22 @@
+package com.designpattern.state.duplicate;
+
+public class State {
+
+	private String value;
+
+	public String getValue() {
+		return value;
+	}
+
+	public void setValue(String value) {
+		this.value = value;
+	}
+	
+	public void method1() {
+		System.out.println("method1 executed");
+	}
+	
+	public void method2() {
+		System.out.println("method2 executed");
+	}
+}
