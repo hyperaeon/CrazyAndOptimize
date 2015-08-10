9f546278cb15a038a6ad13da02b1f0681896a4ab
a588727 <Li, Yong@StateStreet.com>
2015-08-10 17:36:13 +0800

    Modify NewItemActionListener, add algorith->clasic/sort.

---
+++ src/com/algorithm/sort/ShellSort.java
@@ -0,0 +1,77 @@
+package com.algorithm.sort;
+
+public class ShellSort {
+
+	public static void main(String[] args) {
+		int[] data = { 5, 3, 6, 2, 1, 9, 4, 8, 7 };
+		SortUtils.printData(data);
+		shellSort2(data);
+		SortUtils.printData(data);
+	}
+
+	public static void shellSort2(int[] data) {
+		int h = 1;
+		while (h <= data.length) {
+			h = h * 3 + 1;
+		}
+		while (h > 0) {
+			for (int i = h; i < data.length; i += h) {
+				if (data[i] < data[i - h]) {
+					int tmp = data[i];
+					int j = i - h;
+					while (j >= 0 && data[j] > tmp) {
+						data[j + h] = data[j];
+						j -= h;
+					}
+					data[j + h] = tmp;
+				}
+			}
+			h = (h - 1) / 3;
+		}
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	public static void shellSort(int[] data) {
+		int h = 1;
+		while (h <= data.length / 3) {
+			h = 3 * h + 1;
+		}
+		while (h > 0) {
+			for (int i = h; i < data.length; i += h) {
+				if (data[i] < data[i - h]) {
+					int tmp = data[i];
+					int j = i - h;
+					while (j >= 0 && data[j] > tmp) {
+						data[j + h] = data[j];
+						j -= h;
+					}
+					data[j + h] = tmp;//由于上述while循环中j=j-h，所以，这里再加h才变成原来的j
+				}
+			}
+			h = (h - 1) / 3;
+		}
+	}
+}
