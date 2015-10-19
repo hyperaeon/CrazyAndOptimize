a903477827a3bf071ee537eac683a62efe911fef
hzliyong <hzliyong@HIH-D-2784.hz.ntes.domain>
2015-10-19 09:14:23 +0800

    Add effective->chapter7

---
+++ src/com/effective/chapter7/AssertionTest.java
@@ -0,0 +1,18 @@
+package com.effective.chapter7;
+
+public class AssertionTest {
+
+	private static void sort(long[] a, int offset, int size) {
+		assert a != null;
+		assert offset >= 0 && offset <= a.length;
+		assert size >= 0 && size <= a.length - offset;
+		System.out.println("after assertion");
+	}
+
+	public static void main(String[] args) {
+		AssertionTest test = new AssertionTest();
+		long[] a = { 3, 2, 4 };
+		test.sort(a, 0, 2);
+	}
+
+}
