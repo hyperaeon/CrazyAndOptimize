d12ea469b7958568916d454dc885e7fd22359942
李勇 <hzliyong@corp.netease.com>
2017-02-17 20:17:14 +0800

    新增gzip实例。

---
+++ src/com/test/GzipTest.java
@@ -0,0 +1,43 @@
+package com.test;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.util.zip.GZIPInputStream;
+import java.util.zip.GZIPOutputStream;
+
+public class GzipTest {
+
+	public static void main(String[] args) {
+		
+	}
+	
+	public static byte[] gzip(byte[] data) throws Exception {
+		ByteArrayOutputStream bos = new ByteArrayOutputStream();
+		GZIPOutputStream gzip = new GZIPOutputStream(bos);
+		gzip.write(data);
+		gzip.finish();
+		gzip.close();
+		byte[] b = bos.toByteArray();
+		bos.close();
+		return b;
+	}
+	
+	
+	public static byte[] ungzip(byte[] data) throws Exception {
+		ByteArrayInputStream bis = new ByteArrayInputStream(data);
+		GZIPInputStream gzip = new GZIPInputStream(bis);
+		byte[] buf = new byte[1024];
+		int num = -1;
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		while ((num = gzip.read(buf, 0, buf.length)) != -1) {
+			baos.write(buf, 0, num);
+		}
+		gzip.close();
+		bis.close();
+		
+		byte[] b = baos.toByteArray();
+		baos.flush();
+		baos.close();
+		return b;
+	}
+}
