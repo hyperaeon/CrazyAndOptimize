2edd8ee9d1b96525c346b157e1261133e2068237
hzliyong <hzliyong@corp.netease.com>
2017-09-18 12:51:20 +0800

    Prototype

---
+++ src/com/runoob/designpattern/prototype/Circle.java
@@ -0,0 +1,14 @@
+package com.runoob.designpattern.prototype;
+
+public class Circle extends Shape {
+
+	public Circle() {
+		type = "Circle";
+	}
+	
+	@Override
+	void draw() {
+		System.out.println("Inside Circle draw method");
+	}
+
+}
