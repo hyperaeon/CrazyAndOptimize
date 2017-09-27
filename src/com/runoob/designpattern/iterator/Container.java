5c2cbc3a75988d2758dd03751cff63106a863471
hzliyong <hzliyong@corp.netease.com>
2017-09-27 12:48:23 +0800

    iterator

---
+++ src/com/runoob/designpattern/iterator/Container.java
@@ -0,0 +1,6 @@
+package com.runoob.designpattern.iterator;
+
+public interface Container {
+
+	public Iterator getIterator();
+}
---
+++ src/com/runoob/designpattern/iterator/Iterator.java
@@ -0,0 +1,7 @@
+package com.runoob.designpattern.iterator;
+
+public interface Iterator {
+
+	public boolean hasNext();
+	public Object next();
+}
