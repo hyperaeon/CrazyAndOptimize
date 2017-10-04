237b7cf0e38da05d336505f2812b05db9be23a49
hzliyong <hzliyong@corp.netease.com>
2017-10-04 10:38:03 +0800

    add mvc

---
+++ src/com/runoob/designpattern/mvc/MVCPatternDemo.java
@@ -0,0 +1,20 @@
+package com.runoob.designpattern.mvc;
+
+public class MVCPatternDemo {
+
+	public static void main(String[] args) {
+		Student model = retriveStudentFromDatabase();
+		StudentView view = new StudentView();
+		StudentController controller = new StudentController(model, view);
+		controller.updateView();
+		controller.setStudentName("John");
+		controller.updateView();
+	}
+	
+	private static Student retriveStudentFromDatabase() {
+		Student student = new Student();
+		student.setName("Robert");
+		student.setRollNo("10");
+		return student;
+	}
+}
---
+++ src/com/runoob/designpattern/mvc/StudentView.java
@@ -0,0 +1,10 @@
+package com.runoob.designpattern.mvc;
+
+public class StudentView {
+
+	public void printStudentDetails(String studentName, String studentRollNo) {
+		System.out.println("Student:");
+		System.out.println("studentName:" + studentName);
+		System.out.println("Roll No:" + studentRollNo);
+	}
+}
