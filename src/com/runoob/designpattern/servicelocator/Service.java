01d1f6077e489b2ac6f6951a124383c558c60e0a
hzliyong <hzliyong@corp.netease.com>
2017-10-06 16:02:50 +0800

    add service locator

---
+++ src/com/runoob/designpattern/servicelocator/Service.java
@@ -0,0 +1,7 @@
+package com.runoob.designpattern.servicelocator;
+
+public interface Service {
+
+	public String getName();
+	public void execute();
+}
---
+++ src/com/runoob/designpattern/servicelocator/Service2.java
@@ -0,0 +1,15 @@
+package com.runoob.designpattern.servicelocator;
+
+public class Service2 implements Service {
+
+	@Override
+	public String getName() {
+		return "Service2";
+	}
+
+	@Override
+	public void execute() {
+		System.out.println("Executing Service2");
+	}
+
+}
