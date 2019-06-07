0df79d737908836e24b801e5479c9db5541416eb
李勇 <hzliyong@corp.netease.com>
2019-06-07 17:37:14 +0800

    add  adapter

---
+++ src/com/boast/adapter/Forwards.java
@@ -0,0 +1,18 @@
+package com.boast.adapter;
+
+public class Forwards extends Player {
+
+    public Forwards(String name) {
+        super(name);
+    }
+
+    @Override
+    public void attack() {
+        System.out.println("forwards attack");
+    }
+
+    @Override
+    public void defense() {
+        System.out.println("forwards defense");
+    }
+}
