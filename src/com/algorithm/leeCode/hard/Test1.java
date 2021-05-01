a4226d635775296c464d938a761663969a343221
李勇 <hzliyong@corp.netease.com>
2021-05-01 17:39:46 +0800

    add lcode

---
+++ src/com/algorithm/leeCode/hard/Test1.java
@@ -0,0 +1,10 @@
+package com.algorithm.leeCode.hard;
+
+/**
+ * @program: CrazyAndOptimize
+ * @description: sss
+ * @author: hzliyong
+ * @create: 2021-05-01 17:38
+ **/
+public class Test1 {
+}
---
+++ src/com/algorithm/leeCode/simple/Test3.java
@@ -0,0 +1,10 @@
+package com.algorithm.leeCode.simple;
+
+/**
+ * @program: CrazyAndOptimize
+ * @description: sdf
+ * @author: hzliyong
+ * @create: 2021-05-01 17:39
+ **/
+public class Test3 {
+}
